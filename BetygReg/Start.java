a6367496a47cd0ca2758fdbb26c0e1efe86ff8d9
Carl <carl.alvestig@gmail.com>
2016-05-30 17:49:44 +0200

    Tagit bort gammal kod

--- BetygReg/Start.java
+++ BetygReg/Start.java
@@ -2,9 +2,6 @@ package BetygReg;
 
 import java.io.FileNotFoundException;
 
-/**
- * Created by Jaeger on 2016-05-27.
- */
 public class Start {
     public static void main (String[] args) throws FileNotFoundException {
         CourseAdmin courseAdmin = new CourseAdmin();
