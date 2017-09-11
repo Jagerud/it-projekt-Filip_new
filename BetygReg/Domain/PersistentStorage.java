a7499436ffcf466984eaeff148a4e96b45484b52
filip jägerud <filni@student.liu.se>
2017-09-11 14:31:29 +0200

    changed persistent storage to static calls

--- BetygReg/Domain/PersistentStorage.java
+++ BetygReg/Domain/PersistentStorage.java
@@ -4,17 +4,17 @@ import TechnicalServices.DBFacadeSingleton;
 
 public class PersistentStorage {
 
-    private DBFacadeSingleton dbFasad;
+
 
     public PersistentStorage() {
-        dbFasad = dbFasad.getDBFacadeSingleton();
+
     }
 
     public void setGrade(String id, String grade) throws Exception {
-        dbFasad.setStudentGrade(id, grade);
+        DBFacadeSingleton.setStudentGrade(id, grade);
     }
 
     public void getGrade(String id) throws Exception {
-        dbFasad.getStudentGrade(id);
+        DBFacadeSingleton.getStudentGrade(id);
     }
 }
