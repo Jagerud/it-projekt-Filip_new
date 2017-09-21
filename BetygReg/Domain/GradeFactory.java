a174eed6bf5cd1158997aca7c3a2e1a2c8f87f20
filip jägerud <filni@student.liu.se>
2017-09-21 14:29:38 +0200

    starting factory

---
+++ BetygReg/Domain/GradeFactory.java
@@ -0,0 +1,16 @@
+package Domain;
+
+/**
+ * Created by Jaeger on 2017-09-20.
+ */
+public class GradeFactory {
+
+
+    public Grade getGrade (String gradeType) {
+        if (gradeType == null){
+            return null;
+        }
+
+        return null;
+    }
+}
